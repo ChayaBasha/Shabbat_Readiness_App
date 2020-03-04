c172d43b10c3e7716f4f96239924ca09fc4a17a9
ChayaBasha <johanna.blumenthal@gmail.com>
2020-03-04 15:16:15 -0700

    working to get backend running

--- shabbat-shalom/src/app/app-routing.module.ts
+++ shabbat-shalom/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { AppComponent } from './app.component';
+import { ToolBarComponent } from './tool-bar/tool-bar.component';
+import { ShabbatAlertComponent } from './shabbat-alert/shabbat-alert.component';
+import { LoginComponent } from './login/login.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginFormComponent } from './login/login-form/login-form.component';
+import { ChecklistContainerComponent } from './checklist-container/checklist-container.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+{ path: '', component: ShabbatAlertComponent },
+{path: 'login',component: LoginComponent},
+{path: 'checklist', component: ChecklistContainerComponent}, 
+]; 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
