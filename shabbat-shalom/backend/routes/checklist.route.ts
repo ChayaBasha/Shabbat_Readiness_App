ea573d225cc8e58e15352fe93b32ec2f65d50c9b
ChayaBasha <59859876+ChayaBasha@users.noreply.github.com>
2020-03-05 16:07:47 -0700

    Backend checklists (#6)
    * added backend for checklists
    
    * progress in getting form to send to backend

---
+++ shabbat-shalom/backend/routes/checklist.route.ts
@@ -0,0 +1,26 @@
+import * as express from 'express';
+import {
+    addChecklist,
+    viewChecklists,
+    deleteChecklist,
+    updateChecklist
+
+} from '../controllers/checklist.controller';
+
+export const checklistRoutes = express.Router();
+
+// Add checklist
+
+checklistRoutes.post('/addChecklist', addChecklist);
+
+// View checklist
+
+checklistRoutes.get('/checklist', viewChecklists);
+
+// Update recipe
+
+checklistRoutes.put('updateChecklist', updateChecklist )
+
+// Delete recipe
+
+checklistRoutes.delete('/view', deleteChecklist);
