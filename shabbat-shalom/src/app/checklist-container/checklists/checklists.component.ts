6d28742bb957381babf446f3225fad65586b27ec
ChayaBasha <johanna.blumenthal@gmail.com>
2020-03-08 19:19:47 -0700

    added checkist view in checklist container

--- shabbat-shalom/src/app/checklist-container/checklists/checklists.component.ts
+++ shabbat-shalom/src/app/checklist-container/checklists/checklists.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ChecklistModel } from 'backend/models/checklist.model';
+import { ChecklistService } from 'src/app/checklist.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-checklists',
@@ -7,9 +11,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChecklistsComponent implements OnInit {
 
-  constructor() { }
+  checklist$: Observable<ChecklistModel[]>;
+  selectedChecklist: ChecklistModel;
+  
+  constructor(
+    private checklistService: ChecklistService, 
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
   ngOnInit() {
+    this.checklist$ = this.checklistService.getChecklists();
   }
 
+selectChecklist(checklist:ChecklistModel): void {
+  this.selectedChecklist = checklist;
+  // this.router.navigate(['/ingredients',checklist.tasks]);
+}
 }
